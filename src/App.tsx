6b6c9551a3292fe6cc3f72bf36e3fcf4689a328c
TruongDucNghia <nghiatdph14539@fpt.edu.vn>
2022-03-15 22:47:21 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
+import React from 'react';
 import { useState } from 'react'
-import logo from './logo.svg'
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css'
+import AboutPage from './pages/aboutPage';
+import ContactPage from './pages/contactPage';
+import HomePage from './pages/homePage';
+import ProductsPage from './pages/productsPage';
 
-const name: string = "Trương Đức Nghĩa"
-const age: number = 19
-const address: string = "PHÚ THỌ - VN"
+
 function App() {
+
   return (
     <div>
-      <h1>learning react-typescript</h1>
-      <h1>hello world</h1>
-      <h2>hello {name}</h2>
-      <h2>{age}</h2>
-      <h2>{address}</h2>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">React-TypeScript</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink className="nav-link" to="/products">Products</NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink className="nav-link" to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/products' element={<ProductsPage/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/contact' element={<ContactPage/>}/>
+        </Routes>
+      </main>
     </div>
-    )
+  )
 }
 
 export default App
